b791f8e644e220716e814ce5ded194957217a783
David Chen <mvjome@gmail.com>
2012-11-19 17:42:51 +0800

    Add RMMSeg.segment(text="") 直接返回切分好的分词数组

--- lib/rmmseg.rb
+++ lib/rmmseg.rb
@@ -1,3 +1,18 @@
 require File.join(File.dirname(__FILE__), 'rmmseg', 'dictionary')
 require File.join(File.dirname(__FILE__), '..',
                   'ext', 'rmmseg', 'rmmseg')
+
+
+def RMMSeg.segment text = ""
+  return [] if text.strip.size.zero?
+
+  ary = []
+  algor = RMMSeg::Algorithm.new(text)
+  loop do
+    tok = algor.next_token
+    break if tok.nil?
+    ary << tok.text
+  end
+
+  ary
+end
